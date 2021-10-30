d829e87aed3d0a3d3d0a0ee75f9b1d17687e08de
Ejike Victory <victoryejike@gmail.com>
2021-10-30 14:45:50 +0100

    connect valora with celo through qr code scan

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,7 @@
 import { Component } from '@angular/core';
+import WalletConnectProvider from '@walletconnect/web3-provider';
+import Web3 from 'web3';
+import { newKitFromWeb3 } from '@celo/contractkit';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +10,26 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'celoproject';
+
+async connect() {
+  const provider :any = new WalletConnectProvider({
+      rpc: {
+        44787: "https://alfajores-forno.celo-testnet.org",
+        42220: "https://forno.celo.org",
+      },
+    });
+
+    await provider.enable()
+    const web3 = new Web3(provider);
+    let kit = newKitFromWeb3(web3)
+
+    kit.defaultAccount = provider.accounts[0]
+
+    provider.on("accountsChanged", (accounts: []) => {
+      console.log(accounts);
+    });
+}
+
+// connect()
+  
 }
